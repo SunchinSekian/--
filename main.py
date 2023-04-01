893f3a8f0888c00ffae22860df0a55d92c397654
王梓毅 <99599883+SunchinSekian@users.noreply.github.com>
2023-04-01 17:57:32 +0800

    小改

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import threading
 
 
 class Students:
-    def __init__(self, name, classnum) -> None:
+    def __init__(self, name, classnum:int) -> None:
         self.name = name
         self.classnum = classnum
         self.morality = 80
@@ -12,13 +12,22 @@ class Students:
 class QueueBlock:
     def __init__(self) -> None:
         self.students = []
+        self.classnum=0
+    @classmethod
+    def first_add():
+        pass
+    def add_student_to_queueblock():
+        pass
 
 
 class Window:
-    def __init__(self, name: str, priority: int = 0) -> None:
+    """窗口"""
+
+    def __init__(self, name: str, priority: int) -> None:
         self.blocks = []
         self.name = name
         self.priority = priority
+        self.windownum = 0
 
     def __len__(self):
         num = 0
@@ -27,15 +36,34 @@ class Window:
         return num
 
     def add_studets_to_window(self, student):
+        if self.blocks == []:
+            q=QueueBlock()
+        self.windownum += 1
+
+    def consume(self):
         pass
 
 
 class Canting:
     def __init__(self, num) -> None:
-        self.windows = [Window(i) for i in range(num)]
+        self.windows = []
+        self.totalnum = 0
 
-    def get_top_priority(self):
-        return max([i.priority for i in self.windows])
+    def get_total_priority(self):
+        return sum([i.priority for i in self.windows])
+
+    def add_window(self, awindow: Window):
+        def get_priority(window: Window):
+            return window.priority
+        self.windows.append(awindow)
+        self.windows.sort(reverse=True, key=get_priority)
 
     def add_stdents(self, student: Students):
-        a = randint(0,)
+        for i in range(len(self.windows)):
+            a = randint(0, i.priority)
+            if a >= i.windownum:
+                i.add_studets_to_window(student)
+                break
+        else:
+            self.windows[0].add_studets_to_window(student)
+        self.totalnum += 1
